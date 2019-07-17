347985a9842c6b0e8c9c9c1885597b283f6eac4a
lesleycheung <lesleyc@bu.edu>
2019-07-17 13:30:10 -0400

    created build folder

---
+++ build/precache-manifest.d17c8743a1954a0f5fe31fa84d22919b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1333bee7cf79c8f650b7a170b802bb1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aad6e12bf3300548816",
+    "url": "/static/css/main.1893d24a.chunk.css"
+  },
+  {
+    "revision": "d538652694bd439d49af",
+    "url": "/static/js/2.f2f882f0.chunk.js"
+  },
+  {
+    "revision": "1aad6e12bf3300548816",
+    "url": "/static/js/main.785c2750.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
